5b07eaad4332cd177bbe2b479da97c9ce1a04ce8
akielbowicz <30580760+akielbowicz@users.noreply.github.com>
2019-10-16 00:41:17 -0300

    Complete setup definition

--- setup.py
+++ setup.py
@@ -7,3 +7,21 @@ from setuptools.config import read_configuration
 
 # Append cwd for pip 19
 sys.path.append(os.path.abspath("."))
+
+
+################################################################################
+# Programmatically generate some extras combos.
+################################################################################
+extras = read_configuration("setup.cfg")['options']['extras_require']
+
+# Dev is everything
+extras['dev'] = list(chain(*extras.values()))
+
+# All is everything but tests and docs
+exclude_keys = ("tests", "docs", "dev")
+ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
+# Concatenate all the values together for 'all'
+extras['all'] = list(chain.from_iterable(ex_extras.values()))
+
+package_info = get_package_info()
+setup(extras_require=extras, use_scm_version=True, cmdclass=cmdclass, **package_info)
