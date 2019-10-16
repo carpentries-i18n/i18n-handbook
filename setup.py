6bbae613134a119e01f2e919c138ea8702acb3eb
akielbowicz <30580760+akielbowicz@users.noreply.github.com>
2019-10-16 00:23:56 -0300

    Add content

--- setup.py
+++ setup.py
@@ -1,1 +1,9 @@
+#!/usr/bin/env python
+import os
+import sys
 
+from setuptools import setup
+from setuptools.config import read_configuration
+
+# Append cwd for pip 19
+sys.path.append(os.path.abspath("."))
