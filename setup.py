2172df0c60ecbbf20c44c7097cb0516a56454570
akielbowicz <30580760+akielbowicz@users.noreply.github.com>
2019-10-16 00:47:04 -0300

    Remove unnesesary parameter

--- setup.py
+++ setup.py
@@ -24,4 +24,4 @@ ex_extras = dict(filter(lambda i: i[0] not in exclude_keys, extras.items()))
 # Concatenate all the values together for 'all'
 extras['all'] = list(chain.from_iterable(ex_extras.values()))
 
-setup(extras_require=extras, use_scm_version=True, cmdclass=cmdclass)
+setup(extras_require=extras, use_scm_version=True)
